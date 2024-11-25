d90558ded2b1f571686785406d75b1066f8ead27
MaxSlukovskyi <maksym.slukovskyi@gmail.com>
2024-11-25 19:06:44 +0200

    add book repository

---
+++ src/main/java/org/max/booksscraper/repository/BookRepository.java
@@ -0,0 +1,12 @@
+package org.max.booksscraper.repository;
+
+import org.max.booksscraper.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByTitle(String title);
+
+}
