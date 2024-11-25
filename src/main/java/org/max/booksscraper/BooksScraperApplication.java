5f9f14800c7dfc48d88838eb93a62c62d2ad261b
MaxSlukovskyi <maksym.slukovskyi@gmail.com>
2024-11-25 18:34:40 +0200

    init commit

---
+++ src/main/java/org/max/booksscraper/BooksScraperApplication.java
@@ -0,0 +1,13 @@
+package org.max.booksscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksScraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/max/booksscraper/BooksScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package org.max.booksscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
